e73c33ba21c23a38cb3ae4541efe5cadc1f33524
sorniamir <sorniamir2005@gmail.com>
2026-07-25 07:00:27 +0330

    Add files via upload

---
+++ Italiano/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "italian-learning-hub-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      return cachedResponse || fetch(event.request).then(function(networkResponse) {
+        return networkResponse;
+      }).catch(function() {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
